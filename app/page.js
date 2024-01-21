34a7dd02f8e8775c8205c0e19972aee3a40ddafc
AzmuthSzym <simondziadek@gmail.com>
2024-01-21 22:43:16 +0100

    Metamask Connect & Boss health check

--- app/page.js
+++ app/page.js
@@ -1,95 +1,75 @@
-import Image from 'next/image'
+"use client";
+
 import styles from './page.module.css'
+import {useState} from "react";
+//import { ethers } from "ethers";
+const ethers = require("ethers");
+
+//Connect to metamask
+//execute a function
 
 export default function Home() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [signer, setSigner] = useState();
+
+  async function connect()
+  {
+    if(typeof window.ethereum !== "undefined")
+    {
+      try{
+        await ethereum.request({method: "eth_requestAccounts" });
+        setIsConnected(true);
+      }
+      catch(e)
+      {
+        console.log(e);
+      }
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      console.log(accounts);
+    }
+    else
+    {
+      setIsConnected(false);
+    }
+  }
+
+  async function execute()
+  {
+    if (typeof window.ethereum !== "undefined")
+    {
+      const bossAddress = "0x35933cCf6ba86612CE9e763576129F866E4768cd";
+      const bossAbi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[],"name":"GameWon","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"decreaseHealth","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"gameWon","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"health","outputs":[{"internalType":"int256","name":"","type":"int256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}];
+
+      const  provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(bossAddress, bossAbi, signer);
+      try {
+        const healthVal = await contract.health();
+        console.log("Boss Health", healthVal);
+      }
+      catch(error)
+      {
+        console.log(error)
+      }
+    }
+    else
+    {
+      document.getElementById("executeButton").innerHTML = "Please install MetaMask";
+    }
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div className={styles.main}>
+      Hello Frogs
+      {isConnected ? (
+        <>
+          Connected! 
+          <button onClick={() => execute()}>Execute</button>
+        </>
+        ) : (
+        <button onClick={() => connect()}>Connect</button>
+      )}
+    </div>
+    
+  );
 }
